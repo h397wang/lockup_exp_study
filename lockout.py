7b2d023e11671b77a80810c3d2bf5f075a396347
Henry Wang <h397wang@uwaterloo.ca>
2020-12-26 16:42:08 -0500

    remove some outliers, and refactor

--- lockout.py
+++ lockout.py
@@ -39,7 +39,7 @@ import yfinance as yf
 
 AnalysisResults = namedtuple(
 	"AnalysisResults",
-	"period_delta_pcnt"
+	"period_delta_pcnt signal_norm"
 )
 
 
@@ -56,9 +56,9 @@ def format_dt_for_yf(dt):
 	return dt.strftime("%Y-%m-%d")
 
 
-# TODO: visualize shit.
-def normalize_signal(col):
-	pass
+def normalize_signal(df):
+	v0 = df["Close"][0]
+	df["Norm"] = df["Close"].apply(lambda x: 100 * (x - v0) / v0) 
 
 
 class LockoutExpAnalyzer():
@@ -67,22 +67,36 @@ class LockoutExpAnalyzer():
 		self.days_before = days_before
 		self.days_after = days_after
 		self.print_shit = print_shit
+		self.results = []
 
-		results = []
 		for indx, row in self.stocks_df.iterrows():
 			res = self.analyze_stock(row)
-			if res:
-				results.append(res)
+			self.results.append(res)
 
-		period_delta_pcnt_avg = stats.mean(r.period_delta_pcnt for r in results)
-		drop_happens = map(lambda x: 1 if x < 0 else 0, [r.period_delta_pcnt for r in results])
+		# Remove outliers when reporting average.
+		deltas = sorted([r.period_delta_pcnt for r in self.results])
+		cutoff = int(lesn(deltas) / 10)s
+		deltas = deltas[cutoff:-cutoff]
+		assert(len(deltas) < len(self.results))
+		period_delta_pcnt_avg = stats.mean(deltas)
+
+		drop_happens = map(lambda r: 1 if r.period_delta_pcnt < 0 else 0, self.results)
 		correctness = stats.mean(drop_happens)
 
 		print(f"=========================")
 		print(f"Aggregate Summary")
 		print(f"Average Period Delta: {period_delta_pcnt_avg:.1f}%")
 		print(f"How often are we correct about the drop: {100 * correctness:.1f}%")
-		print(f"Number of data points: {len(results)}")
+		print(f"Number of data points: {len(self.results)}")
+	
+	def show_graph(self):
+		for r in self.results:			
+			plt.plot(r.signal_norm)
+		plt.xlabel("Trading Days")
+		plt.ylabel("Percentage Change")
+		plt.title(f"Change From Days [-{self.days_before}, {self.days_after}] of Lockup Expiration")
+		plt.grid(linestyle="--")
+		plt.show()
 
 
 	def analyze_stock(self, row):
@@ -120,37 +134,43 @@ class LockoutExpAnalyzer():
 		start_period_dfq = format_dt_for_dfq(start_dt)
 		days_after_dfq = format_dt_for_dfq(lockout_exp_dt + datetime.timedelta(days=self.days_after))
 
-		# Calculate the mean, and range in the period 1 week pre exp.
-		pre_exp_df = data.query(f"Date <= {lockout_exp_dfq}")
-		pre_exp_highest = pre_exp_df["High"].max()
-		pre_exp_lowest = pre_exp_df["Low"].min()
-		pre_exp_mean = pre_exp_df["Close"].mean()
+		"""
+		pre_exp_period_df = data.query(f"Date <= {lockout_exp_dfq}")
+		pre_exp_highest = pre_exp_period_df["High"].max()
+		pre_exp_lowest = pre_exp_period_df["Low"].min()
+		pre_exp_mean = pre_exp_period_df["Close"].mean()
+		"""
 
 		post_exp_df = data.query(f"Date <= {days_after_dfq}").iloc[-1]
-		post_exp_price_close = post_exp_df["Close"]
-		post_exp_price_high = post_exp_df["High"]
-		post_exp_price_low = post_exp_df["Low"]
+		pre_exp_df = data.iloc[0]
 		
 		# Find the change in closing prices between the start date, and the day after exp.
-		pre_exp_period_start_price = data["Close"][0]
-		period_post_exp_price_delta = post_exp_price_close - pre_exp_period_start_price
-		period_post_exp_price_delta_pcnt = 100 * period_post_exp_price_delta / pre_exp_period_start_price
+		price_delta = post_exp_df["Close"] - pre_exp_df["Close"]
+		price_delta_pcnt = 100 * price_delta / pre_exp_df["Close"]
 		
 		if self.print_shit:
 			print(f"=============================================")
 			print(f"Ticker: {ticker}")
-			print(f"Day After Exp")
-			print(f"\tClose: {post_exp_price_close:.1f}$")
-			print(f"\tHigh: {post_exp_price_high:.1f}$")
-			print(f"\tLow: {post_exp_price_low:.1f}$")
 			
-			print(f"Entry {self.days_before} Days Pre Exp Date")
-			print(f"\tClose: {pre_exp_period_start_price:.1f}$")
-			print(f"\tDelta Amount: {period_post_exp_price_delta:.1f}$")
-			print(f"\tDelta Percent: {period_post_exp_price_delta_pcnt:.1f}%")
+			print(f"{self.days_before} Days Before Exp")
+			print(f"\tClose: {pre_exp_df['Close']:.1f}$")
+			print(f"\tHigh: {pre_exp_df['High']:.1f}$")
+			print(f"\tLow: {pre_exp_df['Low']:.1f}$")
+
+			print(f"Day After Exp")
+			print(f"\tClose: {post_exp_df['Close']:.1f}$")
+			print(f"\tHigh: {post_exp_df['High']:.1f}$")
+			print(f"\tLow: {post_exp_df['Low']:.1f}$")
+
+			print(f"Delta")
+			print(f"\tDelta Amount: {price_delta:.1f}$")
+			print(f"\tDelta Percent: {price_delta_pcnt:.1f}%")
+
+		normalize_signal(data)
 
 		res = AnalysisResults(
-			period_delta_pcnt = period_post_exp_price_delta_pcnt,
+			period_delta_pcnt = price_delta_pcnt,
+			signal_norm = list(data["Norm"]),
 		)
 		return res
 
@@ -179,15 +199,24 @@ def main():
 		action="store_true",
 		help="Print individual stock info."
 	)
+	parser.add_argument(
+		"--show-graph",
+		dest="show_graph",
+		action="store_true",
+		help="Show graph of aggregate normalized signals."
+	)
 	args = parser.parse_args()
 	stocks_df = pd.read_csv(args.csv_file)
 	
-	LockoutExpAnalyzer(
+	lea = LockoutExpAnalyzer(
 		stocks_df,
 		args.days_before,
 		args.days_after,
 		args.print_shit
 	)
+
+	if args.show_graph:
+		lea.show_graph()
 
 
 if __name__ == "__main__":
